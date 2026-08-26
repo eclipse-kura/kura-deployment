1eeb8dc5c6c0cd8746496875cd228a279080dd90
Mattia Dal Ben <matthewdibi@gmail.com>
2026-08-26 08:37:49 +0200

    ci: enable Sonar

--- .jenkins/ci.groovy
+++ .jenkins/ci.groovy
@@ -4,7 +4,7 @@ node {
     continuousIntegrationPipeline(
         buildType: "deploy",
         sonar: [
-            enable: false,
+            enable: true,
             projectKey: "eclipse-kura_kura-deployment",
             tokenId: "sonarcloud-token-kura-deployment",
             exclusions: "tests/**/*,**/*.xml,**/*.yml",
